6e3c9438dfc9f56e57c0bfb6063e58d81a23c9c2
Ariel-levi <38177520+Ariel-levi@users.noreply.github.com>
2023-07-28 18:28:24 +0300

    add access to all to the server

--- routes/config_routes.js
+++ routes/config_routes.js
@@ -11,7 +11,8 @@ exports.corsAccessControl = (app) => {
   app.all("*", function (req, res, next) {
     if (!req.get("Origin")) return next();
 
-    res.set("Access-Control-Allow-Origin", process.env.CLIENT_URL);
+    // res.set("Access-Control-Allow-Origin", process.env.CLIENT_URL);
+    res.set("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, PATCH");
     res.set(
       "Access-Control-Allow-Headers",
